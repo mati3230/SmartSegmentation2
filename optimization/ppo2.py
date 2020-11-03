34296d55303669d9cbc8b1b79f0376a974acbfdf
Marcel Tiator <marcel.tiator@hs-duesseldorf.de>
2020-11-03 11:28:00 +0100

    added documentation

--- optimization/ppo2.py
+++ optimization/ppo2.py
@@ -22,11 +22,54 @@ class PPO2():
             lmbda=0.95,
             entropy_factor=0.01,
             value_factor=0.5,
-            beta=0,
             write_tensorboard=False,
             normalize_returns=True,
             normalize_advantages=True,
             **kwargs):
+        """Constructor. Initializes the target policy.
+
+        Parameters
+        ----------
+        policy_type : type
+            Type of the policy that should be trained to initialize the target
+            policy.
+        policy_args : dictionary
+            Arguments that are used to initialize instances of the policy.
+        n_actions : int
+            Number of available actions.
+        optimizer : tf.keras.optimizers.Optimizer
+            Optimizer such as SGD or ADAM.
+        train_summary_writer : tf.summary.SummaryWriter
+            Summary writer to write tensorboard logs.
+        batch_size : int
+            Number of samples in one batch.
+        global_norm : float
+            Threshold to clip the gradients according to a maximum global norm.
+        check_numerics : boolean
+            If True, an exception is thrown in case of NaN values.
+        gamma : float
+            The discount factor gamma should be between [0, 1].
+        K_epochs : int
+            Number of epochs.
+        eps_clip : float
+            Cliprange epsilon to clip the ratio of the new and old policy in a
+            ppo update.
+        lmbda : float
+            Factor of the generalized advantage estimation that should be
+            between [0, 1].
+        entropy_factor : float
+            Factor to weight the importance of the entropy in the loss
+            function. The higher this factor, the more exploration will be
+            encouraged.
+        value_factor : float
+            Factor to weight the importance of the state value estimation.
+        write_tensorboard : boolean
+            If True, summary will be written.
+        normalize_returns : boolean
+            If True, the expected returns will be normalized in ppo update.
+        normalize_advantages : boolean
+            If True, the advantages will be normalized in ppo update.
+        """
         self.gamma = gamma
         self.n_actions = n_actions
         self.optimizer = optimizer
@@ -39,15 +82,18 @@ class PPO2():
         self.lmbda = lmbda
         self.entropy_factor = entropy_factor
         self.value_factor = value_factor
-        self.beta = beta
+        self.write_tensorboard = write_tensorboard
+        self.normalize_returns = normalize_returns
+        self.normalize_advantages = normalize_advantages
+
+        # initialize the target policy
         pa = policy_args.copy()
         pa["trainable"] = True
         pa["stateful"] = False
         print("target_policy")
         self.target_policy = policy_type(**pa)
-        self.write_tensorboard = write_tensorboard
-        self.normalize_returns = normalize_returns
-        self.normalize_advantages = normalize_advantages
+
+        # print a summary
         print("-----------------------------------")
         print("PPO2: ", locals())
         print("-----------------------------------")
@@ -87,7 +133,7 @@ class PPO2():
         tf.Tensor
             Entropy of the action selection.
         tf.Tensor
-            Overall loss.
+            Total loss.
         tf.Tensor
             Global norm of the gradients.
         tf.Tensor
@@ -95,62 +141,60 @@ class PPO2():
 
         """
         with tf.GradientTape() as tape:
-            # advantages: (B, )
             if self.check_numerics:
                 action_probs = tf.debugging.check_numerics(
                     action_probs, "action_probs")
-            # evaluating actions
+            # evaluate the actions
             logprobs_, state_values, dist_entropy, action_probs_ =\
                 self.target_policy.evaluate(states, actions)
             if self.check_numerics:
-                # tf.print(action_probs_, summarize=100)
                 action_probs_ = tf.debugging.check_numerics(
                     action_probs_, "action_probs_")
-            # logprobs: (B, )
-            logprobs = tf.reshape(logprobs, [tf.shape(logprobs)[0]])
-            # tf.print(tf.shape(logprobs))
+
+            # compute the kullback leibler divergence
             kl_divergence = kl_div(action_probs, action_probs_)
             if self.check_numerics:
                 kl_divergence = tf.debugging.check_numerics(
                     kl_divergence, "kl_divergence")
-            kl_divergence = tf.reduce_mean(kl_divergence)
 
-            # finding the ratio (pi_theta / pi_theta__old)
-            # ratios: (B, )
+            # compute the ratio (pi_theta / pi_theta__old)
+            logprobs = tf.reshape(logprobs, [tf.shape(logprobs)[0]])
             ratios = tf.math.exp(logprobs_ - tf.stop_gradient(logprobs))
             if self.check_numerics:
                 ratios = tf.debugging.check_numerics(ratios, "ratios")
-            # finding surrogate loss
-            # surr1: (B, )
+
+            # compute surrogate loss
             surr1 = ratios * advantages
-            # surr2: (B, )
             surr2 = tf.clip_by_value(
                 ratios, 1-self.eps_clip, 1+self.eps_clip) * advantages
-            # surr_loss: (B, )
             surr_loss = tf.math.minimum(surr1, surr2)
-            # critic_loss: (B, )
+
             critic_loss = tf.math.square(returns - state_values)
             critic_loss = 0.5*tf.reduce_mean(critic_loss)
+
             surr_loss = tf.reduce_mean(surr_loss)
             dist_entropy = tf.reduce_mean(dist_entropy)
+            kl_divergence = tf.reduce_mean(kl_divergence)
+
+            # compute the policy loss
             pi_loss = -(surr_loss + (self.entropy_factor*dist_entropy))
-            # vars_ = self.target_policy.get_vars()
+
+            # gradient update
             vars_ = tape.watched_variables()
+            # compute the total loss
             loss = pi_loss + (self.value_factor*critic_loss)
-            # tf.print(loss)
             global_norm = 0
-            # """
-            # vars_ = tape.watched_variables()
+            # compute the gradients
             grads = tape.gradient(loss, vars_)
+            # norm of the gradients
             global_norm = tf.linalg.global_norm(grads)
-            # for i in range(len(grads)):
-            #    grads[i] = tf.clip_by_value(grads[i], -1e12, 1e12)
 
             if self.global_norm:
                 grads, _ = tf.clip_by_global_norm(
                     grads,
                     self.global_norm,
                     use_norm=global_norm)
+            # apply gradient update
             self.optimizer.apply_gradients(zip(grads, vars_))
             return \
                 surr_loss,\
@@ -161,28 +205,63 @@ class PPO2():
                 kl_divergence
 
     def update(self, transitions, step):
+        """Performes a PPO update.
+
+        Parameters
+        ----------
+        transitions : dictionary
+            The keys of this dictionary are process ids. If only a single
+            process is used, only one element is in the dictionary.
+            The values of the dictionary are lists. A single list contains
+            tuples that are actually the transitions.
+            A tuple consist of:
+                * dictionary that contains informartion from the agent,
+                * the agent action,
+                * the observation according to which the agent selects its
+                  action,
+                * the next observation of the environment,
+                * the reward value and
+                * a flag if the episode is over.
+        step : int
+            Global step number of the ppo update. It is used to log values in
+            tensorboard.
+        """
+        # create some lists from the transitions for the ppo calculation
+        # the expected returns
         returns = []
         advantages = []
         actions = []
+        # list that contains all observations
         all_obs = []
+        # list of all log-probabilities
         logprobs = []
+        # list of all action-probabilities
         action_probs = []
-        # rewards = []
         dones = []
         for trans in transitions.values():
+            # generalized advantage estimation
             gae = 0
+            # previous expected return
             prev_return = 0
+            # previous state value
             prev_value = 0
             for i in reversed(range(len(trans))):
+                # unpack the i-th transition
                 pi_action, action, obs, next_obs, reward, done = trans[i]
+
+                # flip the done values to use the mask as factor
                 mask = 1 - done
                 prev_return = reward + (self.gamma * prev_return * mask)
                 returns.insert(0, prev_return)
+
+                # calculate the generalized advantage estimation
                 value = pi_action["state_value"]
                 delta = reward + (self.gamma * prev_value * mask) - value
                 prev_value = value
                 gae = delta + (self.gamma * self.lmbda * mask * gae)
                 advantages.insert(0, gae)
+
+                # fill the remaining lists (see lists above)
                 logprob = pi_action["logprob"]
                 logprobs.insert(0, logprob)
                 action_prob = pi_action["action_probs"]
@@ -191,26 +270,41 @@ class PPO2():
                 all_obs.insert(0, obs)
                 dones.insert(0, done)
 
-        surr_loss = 0
-        critic_loss = 0
-        dist_entropy = 0
-        loss = 0
-        global_norm = 0
-        kl_divergence = 0
+        # calculate the batches
+        # if using an lstm - store the end indices of each episode
         if self.target_policy.use_lstm:
             dones = np.array(dones, np.bool)
+            # end of the episodes
             batch_indxs = np.where(dones == True)[0]
+            # number of batches
             n_batches = batch_indxs.shape[0]
+            # append the first index
             batch_indxs = np.vstack(
                 (np.array([0], np.int32)[:, None], batch_indxs[:, None]))
             batch_indxs = batch_indxs.reshape((batch_indxs.shape[0], ))
-        else:
+        else: # if using non sequential net
             n_samples = len(returns)
+            # number of batches
             n_batches = math.floor(n_samples / self.batch_size)
             batch_indxs = np.arange(n_samples)
             np.random.shuffle(batch_indxs)
 
+        # define some values that will be logged in tensorboard
+        # surrogate loss
+        surr_loss = 0
+        critic_loss = 0
+        # entropy of the action distribution
+        dist_entropy = 0
+        # total loss value
+        loss = 0
+        # global norm of the gradient
+        global_norm = 0
+        # kullback leibler divergence
+        kl_divergence = 0
+        # the above values will be divided by div
         div = self.K_epochs * n_batches
+
+        # convert to numpy arrays
         returns = np.array(returns, np.float32)
         if self.normalize_returns:
             returns = normalize(returns)
@@ -221,13 +315,19 @@ class PPO2():
         actions = np.array(actions, np.int32)
         logprobs = np.array(logprobs, np.float32)
         action_probs = np.array(action_probs, np.float32)
+
+        # update loop
         for _ in range(self.K_epochs):
             for i in range(n_batches):
+                # calculate the indexes of a batch
                 if self.target_policy.use_lstm:
+                    # we have to take care of the sequence
                     indxs = np.arange(batch_indxs[i], batch_indxs[i+1])
                 else:
                     j = i * self.batch_size
                     indxs = batch_indxs[j:j+self.batch_size]
+
+                # conduct the ppo update
                 surr_loss_,\
                     critic_loss_,\
                     dist_entropy_,\
@@ -239,14 +339,16 @@ class PPO2():
                         actions[indxs],
                         logprobs[indxs],
                         action_probs[indxs])
-                # self.target_policy.reset()
+
+                # update the values to log in tensorboard
                 surr_loss += (surr_loss_ / div)
                 critic_loss += (critic_loss_ / div)
                 dist_entropy += (dist_entropy_ / div)
                 loss += (loss_ / div)
                 global_norm += (global_norm_ / div)
                 kl_divergence += (kl_divergence_ / div)
-        # print("train done")
+
+        # write tensorboard logs
         if self.write_tensorboard:
             with self.train_summary_writer.as_default():
                 tf.summary.scalar("train/loss", loss, step=step)
